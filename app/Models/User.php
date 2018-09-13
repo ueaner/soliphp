7603a039d316a9c7975cf84d6aa5fb821404dfaf
ueaner <ueaner@gmail.com>
2018-09-13 11:08:02 +0800

    Model 中使用相应的 protected 属性自定义数据库连接服务名称、表名、主键

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 class User extends Model
 {
-    public function tableName()
-    {
-        return 'user';
-    }
+    /**
+     * 当前模型访问的数据库连接服务名称
+     */
+    protected $connection = 'db';
+
+    /**
+     * 当前模型操作的表名
+     */
+    protected $table = 'user';
+
+    /**
+     * 当前模型所操作表的主键
+     */
+    protected $primaryKey = 'id';
 }
