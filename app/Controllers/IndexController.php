49548e1f2ab6392d856346dfdcb60fc319f1d154
ueaner <ueaner@gmail.com>
2019-04-02 15:06:26 +0800

    clean code

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -7,19 +7,8 @@ use App\Events\Index as IndexEvents;
 
 class IndexController extends Controller
 {
-    public function __construct()
-    {
-        $eventManager = new EventManager();
-        // 添加 index 事件空间，监听 index.* 的事件
-        $eventManager->attach('index', new IndexEvents());
-
-        $this->setEventManager($eventManager);
-    }
-
     public function index()
     {
-        $this->trigger('index.prepare', 'some data from index');
-
         $this->view->setVar('name', 'Soli');
     }
 }
